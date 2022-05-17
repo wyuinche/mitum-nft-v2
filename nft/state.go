47f31e064ce070726f433f2f568b075c52edb573
Soonkuk.Kang <soonkuk@gmail.com>
2022-05-17 12:03:27 +0900

    fix : add some files must be included

---
+++ nft/state.go
@@ -0,0 +1,50 @@
+package nft
+
+import (
+	"github.com/spikeekips/mitum/base/operation"
+	"github.com/spikeekips/mitum/base/state"
+)
+
+func checkExistsState(
+	key string,
+	getState func(key string) (state.State, bool, error),
+) error {
+	switch _, found, err := getState(key); {
+	case err != nil:
+		return err
+	case !found:
+		return operation.NewBaseReasonError("state, %q does not exist", key)
+	default:
+		return nil
+	}
+}
+
+func existsState(
+	k,
+	name string,
+	getState func(key string) (state.State, bool, error),
+) (state.State, error) {
+	switch st, found, err := getState(k); {
+	case err != nil:
+		return nil, err
+	case !found:
+		return nil, operation.NewBaseReasonError("%s does not exist", name)
+	default:
+		return st, nil
+	}
+}
+
+func notExistsState(
+	k,
+	name string,
+	getState func(key string) (state.State, bool, error),
+) (state.State, error) {
+	switch st, found, err := getState(k); {
+	case err != nil:
+		return nil, err
+	case found:
+		return nil, operation.NewBaseReasonError("%s already exists", name)
+	default:
+		return st, nil
+	}
+}
