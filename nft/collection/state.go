abe2ad91a0ad602b3f61fd50e602591e4260970e
wyuinche <wyuinche.mido@gmail.com>
2022-05-20 16:41:53 +0900

    fix : delete unnecessary codes; about processor
    * Codes for operation processor (agent, collection) are removed
    * Deleted codes will be added with next commit

--- nft/collection/state.go
+++ nft/collection/state.go
@@ -1,76 +1,9 @@
 package collection
 
 import (
-	"fmt"
-	"strings"
-
-	"github.com/pkg/errors"
-	"github.com/spikeekips/mitum/base"
 	"github.com/spikeekips/mitum/base/operation"
 	"github.com/spikeekips/mitum/base/state"
-	"github.com/spikeekips/mitum/util"
 )
-
-var StateKeyAgentsSuffix = ":Agents"
-var StateKeyCollectionPolicySuffix = ":Collection"
-
-func StateKeyAgents(a base.Address) string {
-	return fmt.Sprintf("%s%s", a.String(), StateKeyAgentsSuffix)
-}
-
-func IsStateAgentsKey(key string) bool {
-	return strings.HasSuffix(key, StateKeyAgentsSuffix)
-}
-
-func StateAgentsValue(st state.State) (AgentBox, error) {
-	v := st.Value()
-	if v == nil {
-		return AgentBox{}, util.NotFoundError.Errorf("agent box not found in State")
-	}
-
-	if s, ok := v.Interface().(AgentBox); !ok {
-		return AgentBox{}, errors.Errorf("invalid agent box value found, %T", v.Interface())
-	} else {
-		return s, nil
-	}
-}
-
-func SetStateAgentsValue(st state.State, b AgentBox) (state.State, error) {
-	if uv, err := state.NewHintedValue(b); err != nil {
-		return nil, err
-	} else {
-		return st.SetValue(uv)
-	}
-}
-
-func StateKeyCollectionPolicy(policy CollectionPolicy) string {
-	return fmt.Sprintf("%s%s", policy.Symbol().String(), StateKeyCollectionPolicySuffix)
-}
-
-func IsStateCollectionPolicyKey(key string) bool {
-	return strings.HasSuffix(key, StateKeyCollectionPolicySuffix)
-}
-
-func StateCollectionPolicyValue(st state.State) (CollectionPolicyBox, error) {
-	v := st.Value()
-	if v == nil {
-		return CollectionPolicyBox{}, util.NotFoundError.Errorf("collection policy box not found in State")
-	}
-
-	if s, ok := v.Interface().(CollectionPolicyBox); !ok {
-		return CollectionPolicyBox{}, errors.Errorf("invalid collection policy box value found, %T", v.Interface())
-	} else {
-		return s, nil
-	}
-}
-
-func SetStateCollectionPolicyValue(st state.State, b CollectionPolicyBox) (state.State, error) {
-	if uv, err := state.NewHintedValue(b); err != nil {
-		return nil, err
-	} else {
-		return st.SetValue(uv)
-	}
-}
 
 func checkExistsState(
 	key string,
